417a4ef8c345a9b095fc3823cbff7038940c5217
Julia Cheperis <julia.cheperis@dataart.com>
2025-07-20 17:48:58 +0200

    add files

---
+++ lesson-8-9/main.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+module "s3_backend" {
+  source      = "./modules/s3-backend"
+  bucket_name = "terraform-state-bucket-julia-10110"
+  table_name  = "terraform-locks"
+}
+
+module "vpc" {
+  source             = "./modules/vpc"
+  vpc_cidr_block     = "10.0.0.0/16"
+  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  vpc_name           = "vpc"
+}
+
+module "ecr" {
+  source       = "./modules/ecr"
+  ecr_name     = "lesson-5-ecr"
+  scan_on_push = true
+}
+
+module "eks" {
+  source        = "./modules/eks"
+  cluster_name  = "eks-cluster-demo"
+  subnet_ids    = module.vpc.public_subnets
+  instance_type = "t2.medium"
+  desired_size  = 2
+  max_size      = 3
+  min_size      = 1
+}
+
+data "aws_eks_cluster" "eks" {
+  name       = module.eks.eks_cluster_name
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name       = module.eks.eks_cluster_name
+  depends_on = [module.eks]
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+module "jenkins" {
+  source            = "./modules/jenkins"
+  cluster_name      = module.eks.eks_cluster_name
+  oidc_provider_arn = module.eks.oidc_provider_arn
+  oidc_provider_url = module.eks.oidc_provider_url
+  github_pat        = var.github_pat
+  github_user       = var.github_user
+  github_repo_url   = var.github_repo_url
+  depends_on        = [module.eks]
+
+  providers = {
+    helm       = helm
+    kubernetes = kubernetes
+  }
+}
---
+++ lesson-8-9/variables.tf
@@ -0,0 +1,14 @@
+variable "github_pat" {
+  description = "GitHub Personal Access Token"
+  type        = string
+}
+
+variable "github_user" {
+  description = "GitHub username"
+  type        = string
+}
+
+variable "github_repo_url" {
+  description = "GitHub repository name"
+  type        = string
+}
